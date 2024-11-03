1aa3dd031cf2a2dbf22bf1d519fa26dbb7d7850a
ReturDev <sergiomm50@gmail.com>
2024-11-03 20:05:19 +0100

    refactor: update stored procedure calls in CategoryRepository
    - Modified the updateCategoryName method to accept a category ID as a parameter and call the stored procedure with both ID and name.
    - Updated the updateCategorySummary method to include a category ID in the stored procedure call, ensuring the correct category is updated with the new summary.

--- product-service/src/main/java/com/returdev/product/repositories/CategoryRepository.java
+++ product-service/src/main/java/com/returdev/product/repositories/CategoryRepository.java
@@ -42,22 +42,24 @@ public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> 
     /**
      * Updates the name of a category using a stored procedure.
      *
+     * @param categoryId the unique identifier of the category
      * @param newName the new name for the category
      * @return the updated CategoryEntity
      */
     @Modifying
     @Transactional
-    @Query(value = "CALL updateCategoryName(:name)", nativeQuery = true)
-    CategoryEntity updateCategoryName(@Param("name") String newName);
+    @Query(value = "CALL updateCategoryName(:id, :name)", nativeQuery = true)
+    CategoryEntity updateCategoryName(@Param("id") Long categoryId, @Param("name") String newName);
 
     /**
      * Updates the summary of a category using a stored procedure.
      *
+     * @param categoryId the unique identifier of the category
      * @param newSummary the new summary for the category
      * @return the updated CategoryEntity
      */
     @Modifying
     @Transactional
-    @Query(value = "CALL updateCategorySummary(:summary)", nativeQuery = true)
-    CategoryEntity updateCategorySummary(@Param("summary") String newSummary);
+    @Query(value = "CALL updateCategorySummary(:id ,:summary)", nativeQuery = true)
+    CategoryEntity updateCategorySummary(@Param("id") Long categoryId, @Param("summary") String newSummary);
 }
