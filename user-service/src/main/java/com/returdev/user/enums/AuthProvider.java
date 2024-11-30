f0a382af99f2525d278abff510ac28fcf9978bee
sergiomm50 <sergiomm50@gmail.com>
2024-11-30 22:26:56 +0100

    feat: add AuthProvider enum for supported authentication providers
    - Introduced `AuthProvider` enum to represent supported authentication providers in the system.
    - Currently supports `GOOGLE` and `GITHUB` as authentication methods.
    - This enum is used to identify the provider through which a user is authenticated.

---
+++ user-service/src/main/java/com/returdev/user/enums/AuthProvider.java
@@ -0,0 +1,24 @@
+package com.returdev.user.enums;
+
+/**
+ * Enum representing the different authentication providers supported in the system.
+ * This enum is used to identify the provider through which a user is authenticated.
+ *<p>
+ * Currently, the system supports the following authentication providers:
+ * - GOOGLE
+ * - GITHUB
+ */
+public enum AuthProvider {
+
+    /**
+     * Represents authentication through Google.
+     */
+    GOOGLE,
+
+    /**
+     * Represents authentication through GitHub.
+     */
+    GITHUB
+
+}
+
