0677285c0a7599a4f3e0a0db4c8bf224c07f054d
sergiomm50 <sergiomm50@gmail.com>
2024-12-06 18:49:39 +0100

    feat: add @Validated annotation to AuthProviderService interface
    - Added `@Validated` to the `AuthProviderService` interface to enable validation of method parameters.

--- user-service/src/main/java/com/returdev/user/services/authprovider/AuthProviderService.java
+++ user-service/src/main/java/com/returdev/user/services/authprovider/AuthProviderService.java
@@ -3,6 +3,7 @@ package com.returdev.user.services.authprovider;
 import com.returdev.user.entities.AuthProviderEntity;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
+import org.springframework.validation.annotation.Validated;
 
 
 /**
@@ -13,6 +14,7 @@ import jakarta.validation.constraints.NotNull;
  * (e.g., Google, Facebook, etc.).
  * </p>
  */
+@Validated
 public interface AuthProviderService {
 
     /**
