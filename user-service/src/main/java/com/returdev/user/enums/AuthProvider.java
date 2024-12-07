67822dd6ac1e329d2a07e08ba4f111dc5742a31d
sergiomm50 <sergiomm50@gmail.com>
2024-12-07 12:48:29 +0100

    fix: improve AuthProvider enum handling with custom string parsing
    - Added `@JsonCreator` annotation to `AuthProvider ` enum for custom string conversion.
    - Introduced `fromString` method to handle case-insensitive string values and provide detailed error messages for invalid enum values.
    - Custom exception `InvalidEnumValueException` thrown when an invalid value is provided, with a list of valid enum values included in the exception message.

--- user-service/src/main/java/com/returdev/user/enums/AuthProvider.java
+++ user-service/src/main/java/com/returdev/user/enums/AuthProvider.java
@@ -1,24 +1,50 @@
 package com.returdev.user.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.returdev.user.exceptions.InvalidEnumValueException;
+import com.returdev.user.util.EnumUtil;
+
+import java.util.List;
+
+
 /**
- * Enum representing the different authentication providers supported in the system.
- * This enum is used to identify the provider through which a user is authenticated.
- *<p>
- * Currently, the system supports the following authentication providers:
- * - GOOGLE
- * - GITHUB
+ * Enum representing various authentication providers supported by the system.
  */
 public enum AuthProvider {
 
     /**
      * Represents authentication through Google.
+     * This value is used when the authentication is done via Google accounts.
      */
     GOOGLE,
 
     /**
      * Represents authentication through GitHub.
+     * This value is used when the authentication is done via GitHub accounts.
      */
-    GITHUB
+    GITHUB;
+
+    /**
+     * Converts a string value to an {@link AuthProvider} enum.
+     *
+     * @param value The string representation of an {@link AuthProvider} enum.
+     * @return The corresponding {@link AuthProvider} enum.
+     * @throws IllegalArgumentException If the provided string does not match any valid enum value,
+     *                                  an {@link InvalidEnumValueException} will be thrown.
+     */
+    @JsonCreator
+    public static AuthProvider fromString(String value) throws IllegalArgumentException {
+        try {
+            return AuthProvider.valueOf(value.toUpperCase());
+        } catch (IllegalArgumentException ex) {
+            throw new InvalidEnumValueException(
+                    value,
+                    "validation.invalid_enum_value.message",
+                    EnumUtil.getValidValuesToString(List.of(AuthProvider.values()))
+            );
+        }
+    }
 
 }
+
 
