afa4f601992ef02b022970b20d376cc76033fd16
sergiomm50 <sergiomm50@gmail.com>
2024-12-07 12:47:59 +0100

    fix: improve UserRole enum handling with custom string parsing
    - Added `@JsonCreator` annotation to `UserRole` enum for custom string conversion.
    - Introduced `fromString` method to handle case-insensitive string values and provide detailed error messages for invalid enum values.
    - Custom exception `InvalidEnumValueException` thrown when an invalid value is provided, with a list of valid enum values included in the exception message.

--- user-service/src/main/java/com/returdev/user/enums/UserRole.java
+++ user-service/src/main/java/com/returdev/user/enums/UserRole.java
@@ -1,6 +1,14 @@
 package com.returdev.user.enums;
 
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.returdev.user.exceptions.InvalidEnumValueException;
+import com.returdev.user.util.EnumUtil;
+import org.apache.catalina.User;
+
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Enum representing the different roles a user can have in the system.
  * <p>
@@ -44,6 +52,33 @@ public enum UserRole {
      * The SUPPORT role represents a user with privileges to manage customer inquiries,
      * assist users, and access support-related tools.
      */
-    SUPPORT
+    SUPPORT;
+
+    /**
+     * Converts a string value to an {@link UserRole} enum value.
+     * <p>
+     * This method attempts to parse the provided string value into a corresponding {@link UserRole} enum.
+     * If the value does not match any valid enum value, it throws an {@link InvalidEnumValueException}.
+     * </p>
+     *
+     * @param value The string representation of the {@link UserRole} to be converted.
+     * @return The corresponding {@link UserRole} enum.
+     * @throws IllegalArgumentException If the provided string does not match any valid {@link UserRole} enum value.
+     * @throws InvalidEnumValueException If the provided value is not a valid enum value for {@link UserRole}.
+     */
+    @JsonCreator
+    public static UserRole fromString(String value) throws IllegalArgumentException {
+        try {
+            return UserRole.valueOf(value.toUpperCase());
+        } catch (IllegalArgumentException ex) {
+            throw new InvalidEnumValueException(
+                    value,
+                    "validation.invalid_enum_value.message",
+                    EnumUtil.getValidValuesToString(List.of(UserRole.values()))
+            );
+        }
+    }
+
+
 }
 
