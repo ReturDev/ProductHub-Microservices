c5fcdb3c452875ce64491d082f08bf3588666a3a
sergiomm50 <sergiomm50@gmail.com>
2024-12-07 11:47:40 +0100

    refactor: remove unnecessary id field from AuthProviderResponseDTO
    - Removed the `id` field from `AuthProviderResponseDTO` as it is no longer required for the response representation.

--- user-service/src/main/java/com/returdev/user/dtos/response/AuthProviderResponseDTO.java
+++ user-service/src/main/java/com/returdev/user/dtos/response/AuthProviderResponseDTO.java
@@ -5,13 +5,11 @@ import com.returdev.user.enums.AuthProvider;
 /**
  * Data Transfer Object (DTO) for authentication provider responses.
  *
- * @param id The unique identifier of the authentication provider record.
  * @param providerName The name of the authentication provider (e.g., GOOGLE, FACEBOOK),
  *                     represented as an {@link AuthProvider} enum.
  * @param providerId The unique identifier assigned by the authentication provider for the user.
  */
 public record AuthProviderResponseDTO(
-        Long id,
         AuthProvider providerName,
         String providerId
 ) {}
