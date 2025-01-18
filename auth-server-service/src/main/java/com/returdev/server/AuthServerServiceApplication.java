76619c8fc3655402965c918aba60efde644afcd3
sergiomm50 <sergiomm50@gmail.com>
2025-01-18 13:25:39 +0100

    feat: add authorization server service module to parent microservices project
    - Create authorization server service module within the parent microservices structure.

---
+++ auth-server-service/src/main/java/com/returdev/server/AuthServerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.returdev.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServerServiceApplication.class, args);
+	}
+
+}
