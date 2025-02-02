8f249ae698fb550f27b96041bb436863533dcf49
sergiomm50 <sergiomm50@gmail.com>
2025-02-02 12:54:30 +0100

    feat: add configuration server service module to parent microservices project

---
+++ config-server-service/src/main/java/com/returdev/configuration/ConfigServerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.returdev.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerServiceApplication.class, args);
+	}
+
+}
---
+++ config-server-service/src/test/java/com/returdev/configuration/ConfigServerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.returdev.configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
